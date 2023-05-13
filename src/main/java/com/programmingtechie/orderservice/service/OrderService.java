f88a5a694205049fb96f2a532cfe9d71b2fac7cf
Antony George <antonyjoy.george@gmail.com>
2023-05-13 13:37:28 +0530

    Initial Commit

---
+++ src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.programmingtechie.orderservice.service;
+
+import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.model.Order;
+import com.programmingtechie.orderservice.model.OrderLineItems;
+import com.programmingtechie.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+
+        Order order = Order.builder()
+                .orderNumber(UUID.randomUUID().toString())
+                .orderLineItemsList(orderRequest
+                        .getOrderLineItemsDtoList().stream()
+                        .map(this::mapToDto)
+                        .toList())
+                .build();
+
+        orderRepository.save(order);
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        return OrderLineItems.builder()
+                .price(orderLineItemsDto.getPrice())
+                .quantity(orderLineItemsDto.getQuantity())
+                .skuCode(orderLineItemsDto.getSkuCode())
+                .build();
+    }
+}
